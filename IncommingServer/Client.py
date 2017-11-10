3699892a93fa87e8c29430848f7e33e1633182d2
idoczesler <dodes.nsmx@gmail.com>
2017-11-10 10:48:12 +0200

    Fixed client connection issue. TODO: - Implement more SMTP Protocol requirements. - Introduce login mechanism. - Proceed to make SMTP recipient and client recipient. - Introduce a database for the messages.

--- IncommingServer/Client.py
+++ IncommingServer/Client.py
@@ -1,37 +1,21 @@
-# import smtplib
-# import email.utils
-# from email.mime.text import MIMEText
-#
-# # Create the message
-# msg = MIMEText('This is the body of the message.')
-# msg['To'] = email.utils.formataddr(('Recipient', 'recipient@example.com'))
-# msg['From'] = email.utils.formataddr(('Author', 'author@example.com'))
-# msg['Subject'] = 'Simple test message'
-#
-# server = smtplib.SMTP('127.0.0.1', 55555)
-# server.starttls()
-# server.login('user', 'pass')
-# server.set_debuglevel(True) # show communication with the server
-#
-# try:
-#     server.sendmail('author@example.com', ['recipient@example.com'], msg.as_string())
-# finally:
-#     server.quit()
-
-
 import smtplib
 import email.utils
 from email.mime.text import MIMEText
 
-# Create the message
-msg = MIMEText('This is the body of the message.')
-msg['To'] = email.utils.formataddr(('Recipient', 'recipient@example.com'))
-msg['From'] = email.utils.formataddr(('Author', 'author@example.com'))
-msg['Subject'] = 'Simple test message'
 
-server = smtplib.SMTP('127.0.0.1', 1025)
-server.set_debuglevel(True) # show communication with the server
-try:
-    server.sendmail('author@example.com', ['recipient@example.com'], msg.as_string())
-finally:
-    server.quit()
+if raw_input('Send or receive ? [S/R]') == 'S':
+
+    # Create the message
+    msg = MIMEText('This is the body of the message.')
+    msg['To'] = email.utils.formataddr(('Recipient', 'arbel03@gmail.com'))
+    msg['From'] = email.utils.formataddr(('Author', 'ido@idoar.com'))
+    msg['Subject'] = 'Simple test message'
+    msg.set_payload('Arbel!')
+
+    server = smtplib.SMTP('127.0.0.1', 1025)
+    server.set_debuglevel(True) # show communication with the server
+    try:
+        server.sendmail(email.utils.parseaddr(msg['From'])[1], email.utils.parseaddr(msg['To'])[1], msg.as_string())
+    finally:
+        server.quit()
+
--- IncommingServer/Server.py
+++ IncommingServer/Server.py
@@ -1,52 +1,58 @@
-# from datetime import datetime
-# import asyncore
-# from smtpd import SMTPServer
-#
-#
-# class CustomSMTPServer(SMTPServer):
-#
-#     def process_message(self, peer, mailfrom, rcpttos, data):
-#         print 'Message'
-#         filename = '%s-%d.eml' % (datetime.now().strftime('%Y%m%d%H%M%S'),
-#                 self.no)
-#         f = open(filename, 'w')
-#         f.write(data)
-#         f.close
-#         print '%s saved.' % filename
-#         self.no += 1
-#
-#
-#
-# def run():
-#     server = CustomSMTPServer(('127.0.0.1', 55555), None)
-#     try:
-#         server
-#         asyncore.loop()
-#     except KeyboardInterrupt:
-#         pass
-#
-#
-# if __name__ == '__main__':
-#     run()
-#
-
-
 import smtpd
 import asyncore
 import email
+import email.utils as utils
+import email.generator as generator
+import os
+import smtplib
+import time
 
 
 class CustomSMTPServer(smtpd.SMTPServer):
+
     def process_message(self, peer, mailfrom, rcpttos, data):
-        print 'Receiving message from:', peer
-        print 'Message addressed from:', mailfrom
-        print 'Message addressed to  :', rcpttos
-        print 'Message length        :', len(data)
-        msg = email.message_from_string(data)
-        msg
+        for rcp in rcpttos:
+            if rcp.split('@')[1].split('.')[0] == 'idoar':
+                self.relay_message_to_client(mailfrom, rcp, data)
+            else:
+                self.relay_message_to_smtp(mailfrom, rcp, data)
         return
+
+    @staticmethod
+    def relay_message_to_smtp(mailfrom, rcp, data):
+        # Relay the message to the recipient's smtp servr
+        msg = email.message_from_string(data)
+
+        # TODO: dns parcing of the recipient to get the SMTP server...
+        dom = rcp.split('@')[1].split('.')[0]
+        if dom == 'gmail':
+            print 'Sending to gmail... (%s)' % rcp
+            s = smtplib.SMTP('smtp.gmail.com:587')
+        else:
+            s = smtplib.SMTP('127.0.0.1', 1027)
+        s.set_debuglevel(True)  # show communication with the server
+        try:
+            s.sendmail(email.utils.parseaddr(msg['From'])[1], email.utils.parseaddr(msg['To'])[1], msg.as_string())
+        finally:
+            s.quit()
+
+    @staticmethod
+    def save_message_to_client(mailfrom, rcp, data):
+        client_name = rcp.split('@')[0]
+        client_path = os.getcwd() + '/Clients/' + client_name
+        if not os.path.exists(client_path):
+            os.makedirs(client_path)
+
+        msg = email.message_from_string(data)
+        with open(client_path + '/' + utils.formatdate(time.time()) + '.elm', 'w') as out:
+            gen = generator.Generator(out)
+            gen.flatten(msg)
+
+        return
+
 
 
 server = CustomSMTPServer(('127.0.0.1', 1025), None)
 
+print 'SMTP Relay Server binding @(%s, %s)' % server.addr
 asyncore.loop()
